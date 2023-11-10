3b82e0a9d53bad1fea0706c7e91896d00e77a733
jakenogueira <jakeline.ezfront@mutant.com.br>
2023-11-10 16:07:27 -0300

    feat: screen initial

---
+++ src/pages/register/register.tsx
@@ -0,0 +1,16 @@
+import './register.css';
+
+export default function Register() {
+
+    return (
+        <div className="search">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        teste
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
