39b5f8b83c485d457bd1c1ca7a2dea60072e5d7f
Allyson-Santana <santana.allyson459@gmail.com>
2023-11-10 16:06:07 -0300

    feat: create service service createTemplate

--- src/services.ts
+++ src/services.ts
@@ -2,25 +2,48 @@ const {
     VITE_BASE_API
 } = import.meta.env
 
-export const getTemplates = async () => {
-    const url = `${VITE_BASE_API}/prompt-templates/`
-    
-    fetch(url, {
-        method: "GET",
-        mode: "no-cors",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }).then(response => {
-        return response.json();
-    }).catch(error => {
-        console.error("Get templates: ", error);
+
+export const getTemplates = () => {
+    const url = `${VITE_BASE_API}/prompt-templates/`;
+
+    return new Promise((resolve, reject) => {
+        fetch(url, {
+            method: "GET",
+            mode: "no-cors",
+        }).then(response => {
+            resolve(response.json())
+        }).catch(error => {
+            console.error("Get templates: ", error);
+            reject(error);
+        })
     })
 
 }
 
 
-export const createTemplate = () => { }
+export type createTemplateProps = {
+    content: string
+    variables: Record<string, string>
+}
+export const createTemplate = (props: createTemplateProps) => {
+    const url = `${VITE_BASE_API}/prompt-templates/`;
+
+    return new Promise((resolve, reject) => {
+        fetch(url, {
+            method: "POST",
+            mode: "no-cors",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(props)
+        }).then(response => {
+            resolve(response.json())
+        }).catch(error => {
+            console.error("Get templates: ", error);
+            reject(error);
+        })
+    })
+}
 
 
 export const createConversation = () => { }
