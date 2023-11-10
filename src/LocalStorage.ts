647d22c14edfee90ef2689b388146b72185f835c
Mene.Mateus <mateusboxxx@gmail.com>
2023-11-10 17:27:47 -0300

    feat: add a new method called parseSession

--- src/LocalStorage.ts
+++ src/LocalStorage.ts
@@ -1,15 +1,22 @@
 import { LocalStorageTemplate } from "./types"
 
+export const createSession = (props: LocalStorageTemplate) => {
+   let stringStored = JSON.stringify(props)
+   localStorage.setItem(props.templateId, stringStored);
+}
 
-export const LocalStorage = () => {
-   const createSession = (props: LocalStorageTemplate) => {
-    let stringStored = JSON.stringify(props)
-    localStorage.setItem(props.templateId ,stringStored);
-    }
+export const readSession = (props: LocalStorageTemplate) => {
+   let stringStored = localStorage.getItem(props.templateId)
+   if (stringStored) {
+      return JSON.parse(stringStored);
+   }
+}
 
-    const readSession = (props: LocalStorageTemplate) => {
-    let stringStored = localStorage.getItem(props.templateId)
-    if(stringStored){
-       return JSON.parse(stringStored);
-    }}
+export const parseSession = (props: LocalStorageTemplate) => {
+   let data;
+   let stringStored = localStorage.getItem(props.templateId)
+   if (stringStored) {
+      data = JSON.parse(stringStored);
+      return `Você é um Bot que atua no canal de whatsapp trabalhando para loja ${data.name}.\n ${data.name} é uma loja de ${data.niche} \nO cliente que você está atendendo se chama {nome}, comece o ${data.typeContext}, ${data.details}`
+   }
 }
